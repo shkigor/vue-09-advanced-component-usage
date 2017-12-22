135c772b8b597ed60c7159c90af8ba73775e20a9
Igor Shklyar <igor.shklyar@gmail.com>
2017-12-22 13:39:49 +0200

    118 Setting up the Module Project

---
+++ src/components/Quote.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>A wonderful Quote!</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "quote",
+        data: function () {
+          return {
+            sss: 'sss'
+          }
+        }
+    }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+</style>
