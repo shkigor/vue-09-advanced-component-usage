c3dc6804a092ec102d9d23863caab78a62dba152
Ihor Shkliar <igor.shklyar@gmail.com>
2020-05-20 17:00:22 +0300

    Create new vue CLI 3 default project

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-              <button @click="selectedComponent = 'appQuote'">Quote</button>
-              <button @click="selectedComponent = 'appAuthor'">Author</button>
-              <button @click="selectedComponent = 'appNew'">New</button>
-              <hr>
-              <p>{{ selectedComponent }}</p>
-              <keep-alive>
-                <component :is="selectedComponent">
-                  <p>Default Content</p>
-                  <h2 slot="title">{{ quoteTitle }}</h2>
-                  <p slot="content">A wonderful Quote</p>
-                </component>
-              </keep-alive>
-              <!--<app-quote>-->
-                <!--<h2 slot="title">{{ quoteTitle }}</h2>-->
-                <!--<p>A wonderful Quote</p>-->
-              <!--</app-quote>-->
-            </div>
-        </div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    import Quote from './components/Quote'
-    import Author from './components/Author'
-    import New from './components/New'
+import HelloWorld from './components/HelloWorld.vue'
 
-    export default {
-        data: function () {
-          return {
-            quoteTitle: 'The Quote',
-            selectedComponent: 'appQuote'
-          }
-        },
-        components: {
-          appQuote: Quote,
-          appAuthor: Author,
-          appNew: New
-        }
-    }
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
