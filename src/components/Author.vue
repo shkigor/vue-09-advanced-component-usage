6fd9f29e904f8a61781a674ae5be2abc4c60e0e5
Ihor Shkliar <igor.shklyar@gmail.com>
2020-06-22 21:09:50 +0300

    132. Switching Multiple Components with Dynamic

---
+++ src/components/Author.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>The Author</div>
+</template>
+
+<script>
+    export default {
+        name: "Author"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/New.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>New Quote</div>
+</template>
+
+<script>
+    export default {
+        name: "New"
+    }
+</script>
+
+<style scoped>
+
+</style>
